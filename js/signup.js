520b0f157066ca5d5a92f41da8de9cf1a68c2767
Hafsa Imran <hafsaemran1712@gmail.com>
2026-09-19 16:18:50 +0500

    Update

--- js/signup.js
+++ js/signup.js
@@ -4,11 +4,16 @@ const firstName = document.querySelector("#firstName")
 const lastName = document.querySelector("#lastName")
 const email = document.querySelector("#email")
 const password = document.querySelector("#password")
+const confirmPassword = document.querySelector("#confirmPassword")
 
 signupBtn.addEventListener("click", async (event) => {
     event.preventDefault()
 
     try {
+
+        if(confirmPassword === password){
+
+        }
 
         console.log("signup");
 
@@ -18,13 +23,35 @@ signupBtn.addEventListener("click", async (event) => {
             password: password.value,
         })
 
+        // Agar signup mein error ho
         if (signupError) {
-            console.log(signupError);
+            console.log(error.message);
+
+            Swal.fire({
+                icon: "error",
+                title: "Signup Failed",
+                text: error.message,
+            });
+
+            return;
         }
 
-        console.log(data.id);
+        // ================== Get User ID ==================
 
-      
+        const id = data.user?.id;
+
+        console.log("User ID:", id);
+
+        if (!id) {
+            console.log("User ID not found");
+            return;
+        }
+
+
+        const { error } = await client
+            .from('user_data')
+            .insert({ id: 1, first_name: firstName, last_name: lastName })
+
     } catch (error) {
         console.log(error);
     }
