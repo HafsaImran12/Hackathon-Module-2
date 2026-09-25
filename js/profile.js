d1b659fd76ea13f8d09d19003286df18b8eb2b56
HafsaImran12 <hafsaemran1712@gmail.com>
2026-09-25 00:44:11 -0700

    Update

--- js/profile.js
+++ js/profile.js
@@ -105,3 +105,20 @@ async function getMyRecipesCount() {
   }
 }
 getMyRecipesCount()
+
+let logouts = document.querySelectorAll(".logoutBtn");
+
+logouts.forEach(logout => {
+  logout.addEventListener("click", async () => {
+
+  const { error } = await client.auth.signOut();
+  
+  if (error) {
+    console.log(error.message);
+    return;
+  }
+
+  window.location.href = "../index.html";
+});
+
+});
